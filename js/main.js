48ba3c36b401cefd94b2d99aa42cf9bf9ed94207
GrinLOLZ <plasmgames@gmail.com>
2023-10-22 02:37:34 -0400

    js to html nav bar testing

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const navelement = document.getElementById("navbarr2rs")
+
+var navbar = `
+<nav>
+    <ul class="navcontainer">
+        <li><button class="navbutton">Home</button></li>
+        <li><button class="navbutton">Games</button></li>
+        <li><button class="navbutton">Apps</button></li>
+        <li><button class="navbutton">Socials</button></li>
+        <li><button class="navbutton">Options</button></li>
+    </ul>
+</nav>
+`
+
+document.querySelector("#navbar").insertAdjacentHTML("afterbegin", navbar)
+
+
+//it will work trust 💯, ts dont work
+
