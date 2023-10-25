2b7d7fe298189ffbfa0c5ef2ce9176016a21c4fd
GrinLOLZ <plasmgames@gmail.com>
2023-10-24 23:02:20 -0400

    replaced 😔

---
+++ js/loadgame.js
@@ -0,0 +1,17 @@
+var gametag = window.location.search;
+var iframe = document.getElementById("myIframe")
+if (gametag === '?game=cookieclicker') {
+    const iframe = document.getElementById("myIframe")
+    iframe.src = "games/cookie clicker/index.html"
+    console.log(gametag)
+}
+if (gametag === '?game=jeffbezosiszaddy') {
+    const iframe = document.getElementById("myIframe")
+    console.log(gametag)
+}
+if (gametag === '?game=fleeingthecomplex') {
+    const iframe = document.getElementById("myIframe")
+    iframe.src = "games/fleeingthecomplex/index.html"
+    console.log(gametag)
+}
+                
