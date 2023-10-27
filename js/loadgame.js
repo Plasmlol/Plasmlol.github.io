7ba8b4af3d28821cd240ef799e350e777b633169
GrinLOLZ <plasmgames@gmail.com>
2023-10-27 01:20:35 -0400

    Fullscreen and descriptions

--- js/loadgame.js
+++ js/loadgame.js
@@ -1,17 +1,24 @@
 var gametag = window.location.search;
 var iframe = document.getElementById("myIframe")
+
 if (gametag === '?game=cookieclicker') {
-    const iframe = document.getElementById("myIframe")
-    iframe.src = "games/cookie clicker/index.html"
-    console.log(gametag)
+const iframe = document.getElementById("myIframe")
+iframe.src = "games/cookie clicker/index.html"
+document.getElementById("a").innerHTML = "You should try this irl";
+document.getElementById("b").innerHTML = "Controls: <br> Just click lil bro";
+console.log(gametag)
 }
 if (gametag === '?game=jeffbezosiszaddy') {
-    const iframe = document.getElementById("myIframe")
-    console.log(gametag)
+const iframe = document.getElementById("myIframe")
+iframe.src = "games/youarejeffbezos/index.html"
+document.getElementById("text").innerHTML = "Would this be lucky to wake up as jeff bezos of be unluckey because of how much you need to manage";
+console.log(gametag)
 }
+
 if (gametag === '?game=fleeingthecomplex') {
-    const iframe = document.getElementById("myIframe")
-    iframe.src = "games/fleeingthecomplex/index.html"
-    console.log(gametag)
+const iframe = document.getElementById("myIframe")
+iframe.src = "games/fleeingthecomplex/index.html"
+document.getElementById("text").innerHTML = "ok what the flip";
 }
+
                 
--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const navelement = document.getElementById("navbarr2rs")
 var navbar = `
 <nav>
     <ul class="navcontainer">
-    <img src="imgs/favicon.png" style=" width:50px; height: 50px;">
-        <h1 class="name">Plasm</h1>
-        <li class="navlist" style="height: 15%;"><a href="index.html"><button class="navbutton">Home</button></a></li>
-        <li class="navlist" style="height: 15%;"><a href="games.html"><button class="navbutton">Games</button></a></li>
-        <li class="navlist" style="height: 15%;"><button class="navbutton">Apps</button></li>
-        <li class="navlist" style="height: 15%;"><button class="navbutton">Socials</button></li>
-        <li class="navlist" style="height: 15%;"><a href="settings.html"><button class="navbutton">Options</button></a></li>
-        <li class="navlist" style="height: 15%;"><a href="updates.html"><button class="navbutton">Updates</button></a></li>
+            <img src="imgs/favicon.png" style=" width:50px; height: 50px;">
+            <h1 class="name">Plasm</h1>
+            <li class="navlist" style="height: 15%;"><a href="index.html"><button class="navbutton">Home</button></a></li>
+            <li class="navlist" style="height: 15%;"><a href="games.html"><button class="navbutton">Games</button></a></li>
+            <li class="navlist" style="height: 15%;"><button class="navbutton">Apps</button></li>
+            <li class="navlist" style="height: 15%;"><button class="navbutton">Socials</button></li>
+            <li class="navlist" style="height: 15%;"><a href="settings.html"><button class="navbutton">Options</button></a></li>
+            <li class="navlist" style="height: 15%;"><a href="updates.html"><button class="navbutton">Updates</button></a></li>
     </ul>
 </nav>
 `
@@ -18,5 +18,5 @@ var navbar = `
 document.querySelector("#navbar").insertAdjacentHTML("afterbegin", navbar)
 
 
-//it will work trust 💯, ts dont work
+//it will work trust 💯, ts dont work why are my ball itchy
 
